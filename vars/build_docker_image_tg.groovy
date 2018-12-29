f0f55ec29ad0fd490e7d41c38d9345b0c851f800
mdshoaib707 <mdshoaib707@gmail.com>
2018-12-29 13:40:28 +0530

    Updated groovy script

--- vars/build_docker_image_tg.groovy
+++ vars/build_docker_image_tg.groovy
@@ -5,23 +5,19 @@ def call(def agent, def branch, def project) {
       checkout([$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '', url: 'https://github.com/mdshoaib707/code-tw.git']]])
     }
 
-    stage ('Docker Image Build') {
+    stage ('Build Image') {
       sh """
       cd ${WORKSPACE}
-      echo $pwd
-      docker rm \$(docker ps -aq) -f || true
-      docker rmi \$(docker images -aq) -f || true
-      docker build -t jenkins:latest .
       """
+      app = docker.build("mdshoaib707/mediwiki-tg")
     }
 
-    stage ('Docker Image Push') {
-      sh """
-      docker tag jenkins:latest mdshoaib707/mediawiki-tg:${BUILD_NUMBER}
-      docker tag jenkins:latest mdshoaib707/mediawiki-tg:latest
-      docker push mdshoaib707/mediawiki-tg:${BUILD_NUMBER}
-      docker push mdshoaib707/mediawiki-tg:latest
-      """
+    stage ('Push Image') {
+      docker.withRegistry('https://registry.hub.docker.com', 'docker-hub-credentials') {
+        app.push("${env.BUILD_NUMBER}")
+        app.push("latest")
+      }
+      
     }
   }
 }
