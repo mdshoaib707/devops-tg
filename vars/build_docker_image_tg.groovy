215e2a2ea24d6937aaee57bc84ae7c2ab8b020d0
mdshoaib707 <mdshoaib707@gmail.com>
2018-12-29 14:19:23 +0530

    Added helm steps to deploy in k8s

--- vars/build_docker_image_tg.groovy
+++ vars/build_docker_image_tg.groovy
@@ -1,4 +1,4 @@
-def call(def agent, def branch, def project) {
+def call(def agent, def branch, def project, def APPENV, def DEVOPSBRANCH, def APPPROJECT) {
   node(agent) {
     stage ('Checkout code') {
       cleanWs()
@@ -27,6 +27,30 @@ def call(def agent, def branch, def project) {
       docker rm \$(docker ps -aq) -f || true
       docker rmi \$(docker images -aq) -f || true
       """
+    }
+    
+    stage ('Checkout Helm Code') {
+      cleanWs()
+       checkout([$class: 'GitSCM', branches: [[name: DEVOPSBRANCH]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '', url: 'https://github.com/mdshoaib707/devops-tg.git']]])
+    }
+    
+    stage ('Deploy Mediawiki app in k8s') {
+      sh """
+      export KUBECONFIG=/home/ubuntu/.kube/config
+      echo $pwd
+      cd ${WORKSPACE}/helm/${APPPROJECT}
+      DEPLOYED=\$(helm list | grep -E ${APPPROJECT} | grep DEPLOYED | wc -l)
+      
+      if [ \${DEPLOYED} = 0 ]; then
+        helm install --name ${APPPROJECT} -f values-${APPENV} ${APPPROJECT}
+        echo Deployed!!!
+      else
+        helm upgrade -f values-${APPENV} ${APPPROJECT} ${APPPROJECT}
+        echo Deployed!!!
+      fi
+      
+      """
+      
     }
   }
 }
